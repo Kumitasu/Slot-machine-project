9a21bc25c21394d1ea3d7cd935fa06182789e06e
Kumitasu <grzybolym@gmail.com>
2022-10-06 15:04:35 +0200

    wallet js

--- wallet.js
+++ wallet.js
@@ -1,27 +1,34 @@
 class Wallet {
-  constructor(money) {
-    let _money = money;
-
-    this.getWalletValue = () => _money;
-
-    this.checkCanPlay = (value) => {
+    constructor(money) {
+     let _money = money;
+     //pobieranie aktualnej zawartości portfela
+     this.getWalletValue = () => _money;
+   
+     //Sprawdzanie czy użytkonik ma odpowiednią ilość środków
+   
+     this.checkCanPlay = value => {
       if (_money >= value) return true;
       return false;
-    };
-
-    this.changeWallet = (value, type = "+") => {
-        if(typeof value === "number" && !isNaN(value)) {
-            if (type === "+"){
-                return _money += value;
-            } else if (type === "-"){
-                return _money -= value;
-        } else {
-            throw new Error("Invalid wallet value")
-        } 
-    } else {
-            console.log(typeof value);
-        }
-  }
-}
-
-const wallet = new Wallet(200)
+     }
+   
+     this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+       if (type === "+") {
+        return _money += value;
+       } else if (type === "-") {
+        return _money -= value;
+       } else {
+        throw new Error("bad operation")
+       }
+   
+      } else {
+       console.log(typeof value);
+       throw new Error("bad sign")
+      }
+     }
+   
+    }
+   
+   }
+   
+   const wallet = new Wallet(200)
