2ba1f6f8ea304f76a8d065daa65757e3df76ddd3
Kumitasu <grzybolym@gmail.com>
2022-10-06 15:03:05 +0200

    js

--- wallet.js
+++ wallet.js
@@ -4,11 +4,24 @@ class Wallet {
 
     this.getWalletValue = () => _money;
 
-    this.checkCanPlay = value => {
-        if(_money >= value) return true;
-        return false; 
-    }
+    this.checkCanPlay = (value) => {
+      if (_money >= value) return true;
+      return false;
+    };
+
+    this.changeWallet = (value, type = "+") => {
+        if(typeof value === "number" && !isNaN(value)) {
+            if (type === "+"){
+                return _money += value;
+            } else if (type === "-"){
+                return _money -= value;
+        } else {
+            throw new Error("Invalid wallet value")
+        } 
+    } else {
+            console.log(typeof value);
+        }
   }
 }
 
-const wallet = new Wallet(200);
+const wallet = new Wallet(200)
