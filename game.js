89fac9cd6c45e3c2f3f6f32dffa42c66d2eaee74
Kumitasu <grzybolym@gmail.com>
2022-10-07 13:57:08 +0200

    js

--- game.js
+++ game.js
@@ -40,11 +40,19 @@ class Statistics {
 
     addGameToStatistics(win, bid){
         let gameResult ={
-            win: win,
-            bid: bid
+            win,
+            bid
         }
         console.log(gameResult);
         this.gameResults.push(gameResult)
+    }
+
+    showGameStatistics(){
+        let games = this.gameResultslength
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length;
+        console.log(wins, losses);
+        return[games, wins, losses]
     }
 
 }
