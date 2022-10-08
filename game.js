12fb787b38922660685e02139c132724356f60ed
Kumitasu <grzybolym@gmail.com>
2022-10-08 11:59:30 +0200

    Result class

--- game.js
+++ game.js
@@ -80,4 +80,18 @@ class Draw {
   }
 }
 
-const draw = new Draw()
+//const draw = new Draw()
+
+
+// Result Class
+
+class Result {
+  static moneyWinInGame(result, bid){
+    if(result) return 3 * bid;
+    else return 0;
+    
+  }
+}
+
+Result.moneyWinInGame(true, 10)
+
