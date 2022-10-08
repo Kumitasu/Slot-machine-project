e1a9d6aad8e824529ce55c4d51bef40a6763e600
Kumitasu <grzybolym@gmail.com>
2022-10-08 11:22:42 +0200

    draw class

--- game.js
+++ game.js
@@ -64,7 +64,15 @@ const stats = new Statistics();
 class Draw {
   constructor() {
     this.options = ['red', 'green', 'blue'];
-    let _result = ['red', 'green', 'blue'];
+    let _result = this.drawResult();
     this.getDrawResult = () => _result;
+  }
+
+  drawResult() {
+    let colors = [];
+    for (let i =0; i < this.options.length; i++) {
+      const index = Math.floor(Math.random() * this.options.length)
+    }
+    return colors;
   }
 }
