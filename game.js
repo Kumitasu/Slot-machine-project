c3c34ca71e1b1b35ccfca24d3b01a4e59f85d7e6
Kumitasu <grzybolym@gmail.com>
2022-10-08 11:25:28 +0200

    draw class

--- game.js
+++ game.js
@@ -72,7 +72,12 @@ class Draw {
     let colors = [];
     for (let i =0; i < this.options.length; i++) {
       const index = Math.floor(Math.random() * this.options.length)
+      const color = this.options[index]
+      console.log(color);
+      colors.push(color);
     }
     return colors;
   }
 }
+
+const draw = new Draw()
