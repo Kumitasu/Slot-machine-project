96fc29e60028d7e2dcc26ec7cb3d29bce1c22a41
Kumitasu <grzybolym@gmail.com>
2022-10-09 19:44:52 +0200

    js

--- game.js
+++ game.js
@@ -103,8 +103,17 @@ class Result {
 // Game Class
 
 class Game {
-  constructor(options){
-
+  constructor(){
+    this.stats = new Statistics();
+    this.wallet = new Wallet(100);
+    document.getElementById('start').addEventListener('click', this.startGame);
+    this.spanWallet = document.querySelector('.panel spanWallet');
+    this.boards = document.querySelectorAll('div.color');
+    this.inputBid = document.getElementById('bid');
+    this.spanResult = document.querySelector('.score span.result');
+    this.spanGames = document.querySelector('.score span.number');
+    this.spanWins = document.querySelector('.score span.win');
+    this.spanLosses = document.querySelector('.score span.loss');
   }
 
   render(){
@@ -112,6 +121,6 @@ class Game {
   }
 
   startGame(){
-    
+
   }
 }
